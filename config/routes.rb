ae986c44c78d8b60134382b223803db731712e8c
crisnoel <crisnoel@hotmail.com>
2018-05-09 19:20:43 -0600

    implementacion de bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :events
+  get 'events/:id/register', to: 'events#register', as: 'register_to_event'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
