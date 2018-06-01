373322c244b18207565d7cd3840b51fbf817cb32
crisnoel <crisnoel@hotmail.com>
2018-05-31 20:33:37 -0600

    configurar ruta para correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { 
+    host: ENV['MAILER_HOST'], 
+    port: 443
+   }
 
 end
