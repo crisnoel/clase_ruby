c594c10ce158d76dfde41e4866bb04c5b03ac981
crisnoel <crisnoel@hotmail.com>
2018-05-31 20:01:54 -0600

    heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -20,12 +20,12 @@ module Meeho
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'hotmail.com',
-      user_name: 'crisnoel',
-      password: '123467890',
-      authentication: 'plain',
+      address: ENV['EMAIL_ADDRESS'],
+      port: ENV['EMAIL_PORT'],
+      domain: ENV['EMAIL_DOMAIN'],
+      user_name: ENV['EMAIL_USER_NAME'],
+      password: ENV['EMAIL_PASSWORD'],
+      authentication: ENV['EMAIL_AUTHENTICATION'],
       enable_starttls_auto: true
     }    
 
