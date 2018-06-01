57fdd25e2e7bc41630dd7b62a0b47d765312a044
crisnoel <crisnoel@hotmail.com>
2018-05-31 20:38:59 -0600

    error en una letra require

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,12 +3,12 @@ class RegistrationsController < Devise::RegistrationsController
     private
 
     def sign_up_params
-        params.requiere(:user).permit(:first_name, :last_name, :date_of_birth, 
+        params.require(:user).permit(:first_name, :last_name, :date_of_birth, 
         :email, :password, :password_confirmation)
     end
 
     def account_update_params
-        params.requiere(:user).permit(:first_name, :last_name, :date_of_birth,
+        params.require(:user).permit(:first_name, :last_name, :date_of_birth,
         :email, :password, :password_confirmation, :current_password)        
     end
     
